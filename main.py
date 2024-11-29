693dc0219c940a5d37d5de0b1910cab66bd0b931
AzureAD\ReemMarji <reem.marji@gotocme.com>
2024-11-29 12:44:35 +0200

    Add tables

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import glob
 paths = glob.glob("Invoices/*.xlsx")
 
 for path in paths:
-    df = pd.read_excel(path, sheet_name="Sheet 1")
 
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
@@ -18,7 +17,30 @@ for path in paths:
     pdf.cell(w=50, h=8, txt=f"Invoice nr. {filename}", ln=1)
 
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Date {Date}")
+    pdf.cell(w=50, h=8, txt=f"Date {Date}", ln=1)
+
+    df = pd.read_excel(path, sheet_name="Sheet 1")
+
+
+    columns = df.columns
+    columns = [c.replace("_", " ").title() for c in columns]
+    pdf.set_font(family="Times", style="B", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=columns[0], border=1)
+    pdf.cell(w=60, h=8, txt=columns[1], border=1)
+    pdf.cell(w=40, h=8, txt=columns[2], border=1)
+    pdf.cell(w=30, h=8, txt=columns[3], border=1)
+    pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
+
+    
+    for index, row in df.iterrows(): 
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=60, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=40, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
 
     pdf.output(f"PDFs/{filename}.pdf")
     
