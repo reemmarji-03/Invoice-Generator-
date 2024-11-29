fe4f6a340a2c90c459c58845312b3303ac1ba3a2
AzureAD\ReemMarji <reem.marji@gotocme.com>
2024-11-29 11:10:48 +0200

    Added Invoice Date

--- main.py
+++ main.py
@@ -7,12 +7,18 @@ paths = glob.glob("Invoices/*.xlsx")
 
 for path in paths:
     df = pd.read_excel(path, sheet_name="Sheet 1")
+
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
 
     filename = path[9:14]
+    Date = path[15:24]
 
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice nr. {filename}")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr. {filename}", ln=1)
+
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Date {Date}")
+
     pdf.output(f"PDFs/{filename}.pdf")
     
